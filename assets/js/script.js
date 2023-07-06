93cffe1fb716a0a54e3116eaff70b480591e1785
Juno & Ice <70862998+JunoAndIce@users.noreply.github.com>
2023-07-06 11:40:30 -0500

    HTML header init, more css work done for header

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+// var timer = 60;
+// var quizzing = false;
+
+
+
+
+
+
+
+// function timeDown(){
+//   while (timer > 0 && quizzing) {
+//     timer --;
+
+//     if (timer == 0) {
+//       endQuiz();
+//       quizzing = !quizzing;
+//       return timer;
+//     }
+//   }
+// }

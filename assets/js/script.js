33dec199224f29567c90c53a2aa621a438f00992
Juno & Ice <70862998+JunoAndIce@users.noreply.github.com>
2023-07-06 17:04:00 -0500

    dark theme fully functioning.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,32 @@
-// var timer = 60;
-// var quizzing = false;
 
+// Credits to Cortez on codepen.io 
 
+var toggleSwitch = document.querySelector('.dark-theme input[type="checkbox"]');
+var currentTheme = localStorage.getItem('theme');
 
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+  
+    if (currentTheme === 'dark') {
+      // Checks the checkbox element
+        toggleSwitch.checked = true;
+    }
+}
 
+// function to change theme.
+function switchTheme(event) {
 
+  if (event.target.checked) {
+    // finds the data-theme attribute in css and sets it's value to dark 
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
+  }
 
+  else {        
+    // finds the data-theme attribute in css and sets it's value to light 
+    document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
+  }    
+}
 
-// function timeDown(){
-//   while (timer > 0 && quizzing) {
-//     timer --;
-
-//     if (timer == 0) {
-//       endQuiz();
-//       quizzing = !quizzing;
-//       return timer;
-//     }
-//   }
-// }
+toggleSwitch.addEventListener('change', switchTheme, false);

8e4d7081bbe853f2ab091f8d7726099af17f0fc1
Juno & Ice <70862998+JunoAndIce@users.noreply.github.com>
2023-07-06 19:40:35 -0500

    naming conventions changed.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,4 @@
-
-// Credits to Cortez on codepen.io 
-
-var toggleSwitch = document.querySelector('.dark-theme input[type="checkbox"]');
-var currentTheme = localStorage.getItem('theme');
-
-if (currentTheme) {
-    document.documentElement.setAttribute('data-theme', currentTheme);
-  
-    if (currentTheme === 'dark') {
-      // Checks the checkbox element
-        toggleSwitch.checked = true;
-    }
-}
-
-// function to change theme.
-function switchTheme(event) {
-
-  if (event.target.checked) {
-    // finds the data-theme attribute in css and sets it's value to dark 
-    document.documentElement.setAttribute('data-theme', 'dark');
-    localStorage.setItem('theme', 'dark');
-  }
-
-  else {        
-    // finds the data-theme attribute in css and sets it's value to light 
-    document.documentElement.setAttribute('data-theme', 'light');
-    localStorage.setItem('theme', 'light');
-  }    
-}
-
-toggleSwitch.addEventListener('change', switchTheme, false);
+// when the user clicks start game, a new card arises with questions
+//and the timer begins counting down
+// when the user selects an answer, they're shown the correct answer.
+// If correct answer isn't chosen, it is highlighted.

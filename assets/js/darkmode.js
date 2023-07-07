a53374af0e9c0b669eb1a9d3a20534b6d5d8792b
Juno & Ice <70862998+JunoAndIce@users.noreply.github.com>
2023-07-06 19:40:11 -0500

    new darkmode.js, and darkmode code moved to it.

---
+++ assets/js/darkmode.js
@@ -0,0 +1,34 @@
+
+// Credits to Cortez on codepen.io 
+// Dark Mod Code 
+var toggleSwitch = document.querySelector('.dark-theme input[type="checkbox"]');
+var currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+  
+    if (currentTheme === 'dark') {
+      // Checks the checkbox element
+        toggleSwitch.checked = true;
+    }
+}
+
+// function to change theme.
+function switchTheme(event) {
+
+  if (event.target.checked) {
+    // finds the data-theme attribute in css and sets it's value to dark 
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
+  }
+
+  else {        
+    // finds the data-theme attribute in css and sets it's value to light 
+    document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
+  }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+

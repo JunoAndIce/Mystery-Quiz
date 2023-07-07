7b0714f31085d6009dbef3d25363b3039808ff9e
Juno & Ice <70862998+JunoAndIce@users.noreply.github.com>
2023-07-07 14:20:27 -0500

    Added next button, and added more js functionality for selected answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,28 @@
 // If correct answer isn't chosen, it is highlighted.
 // WHen the correct answer is chosen, the test moves to the next question
 // When the timer runs out, the test ends immediately and score is recorded
+var start_btn = document.getElementById("start-btn");
+var header = document.getElementById("main-header");
+var quiz_info = document.getElementById("quiz-info");
+var border_line = document.getElementById("border-line");
+var score_ctn = document.getElementById("score-container");
+var points = document.getElementById("points_counter");
+var time_ctn  = document.getElementById("time-container");
+var timer_secs  = document.getElementById("timer_secs");
+var question_sec = document.getElementById("question-sec");
+var question_text = document.getElementById("question-text");
+var answer_btn = document.getElementById("quiz-answers");
+var nextButton = document.getElementById("next-btn");
+
+
+var timeValue = 60;
+var userScore = 0;
+var counter;
+var currentQuizQuestion = 0;
 
 var questions = [
   {
-    question: "Two black men visited Paris in 2012 and performed their hit song over 12 times in a row. Who were these men?",
+    question: "Two famous hip-hop artist visited Paris in 2012 and performed their hit song over 12 times in a row. Who were these artist?",
     answers: [
       {text: "Lil Yachty and Drake", correct: false},
         {text: "Snoop Dogg and Dr.Dre", correct: false},
@@ -58,23 +76,6 @@ var questions = [
 ]
 
 
-var start_btn = document.getElementById("start-btn");
-var header = document.getElementById("main-header");
-var quiz_info = document.getElementById("quiz-info");
-var border_line = document.getElementById("border-line");
-var score_ctn = document.getElementById("score-container");
-var time_ctn  = document.getElementById("time-container");
-var timer_secs  = document.getElementById("timer_secs");
-var question_sec = document.getElementById("question-sec");
-var question_text = document.getElementById("question-text");
-var answer_btn = document.getElementById("quiz-answers");
-
-
-var timeValue = 60;
-var userScore = 0;
-var counter;
-var currentQuizQuestion = 0;
-
 // Quiz initialization. Removes main page elements and shows quiz elements
 function startQuiz(){
   start_btn.style.display = "none";
@@ -94,26 +95,32 @@ function startQuiz(){
 }
 
 function showQuestions(){
+
+  // removes placeholder question and answers.
   resetState();
+
   var currentQuestion = questions[currentQuizQuestion];
   var questionNo = currentQuizQuestion + 1;
 
+  // Adds number before question text
   question_text.innerHTML = questionNo + ". " + currentQuestion.question;
 
+  // Creates a button for each answer choice in the questions object
   currentQuestion.answers.forEach(answer => {
     var button = document.createElement("button");
     button.innerHTML = answer.text;
     button.classList.add("btn");
     answer_btn.appendChild(button);
 
-    // if (answer.correct) {
-    //   button.dataset.correct = answer.correct;
-    // }
-    // button.addEventListener("click", selectAnswer);
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
   });
 }
 
 function resetState() {
+  nextButton.style.display = "none";
   while(answer_btn.firstChild){
     answer_btn.removeChild(answer_btn.firstChild);
   }
@@ -128,31 +135,49 @@ function startTimer(time) {
     time--; //decrement the time value
 
     if (time < 10){
-      timer_secs.style.transition = "color 2s";
+      timer_secs.style.transition = "color 1.5s";
       timer_secs.style.color = "red";
     }
 
     if (time < 0) {
       clearInterval(counter);
       console.log("Time's up!")
+      // endQuiz();
     }
   }
 }
 
 // Selected answer class. Gives color to correct and incorrect
-// function selectAnswer(event) {
-//   var selectedAnswer = event.target;
-//   var isCorrect = selectedAnswer.dataset.correct === "true";
+function selectAnswer(event) {
+  var selectedAnswer = event.target;
+  var isCorrect = selectedAnswer.dataset.correct === "true";
 
-//   if (isCorrect) {
-//     selectedBtn.classList.add("correct");
-//   } 
-//   else {
-//     selectedBtn.classList.add("incorrect");
-//   }
-// }
+  if (isCorrect) {
+    console.log("correct answer chosen")
+    selectedAnswer.classList.add("correct");
+    userScore+=20;
+    points.textContent = userScore;
+  } 
+  else {
+    console.log("wrong answer chosen")
+    selectedAnswer.classList.add("incorrect");
+  }
 
+  // create an array for the buttons and run a foreach loop on it 
+  // Credits: GreatStack on YT 
+  Array.from(answer_btn.children).forEach(button=>{
+    if(button.dataset.correct === "true"){
+      button.classList.add("correct");
 
+    }
+    button.disabled = true;
+
+  });
+  nextButton.style.display = "block";
+}
+
+// next button functionality 
+// nextButton.addEventListener
 
 
 
